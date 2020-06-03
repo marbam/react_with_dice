83db32568fcc9e6db286fa3a2148ed460a7363eb
Martin Bampton <martinbampton@hotmail.com>
2020-06-03 16:32:14 +0100

    Removing redundant commented methods, changing App from class to functional component

--- public/js/app.js
+++ public/js/app.js
@@ -69293,116 +69293,28 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _Die_Die__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Die/Die */ "./resources/js/components/Die/Die.js");
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
 
 
-
-var App = /*#__PURE__*/function (_Component) {
-  _inherits(App, _Component);
-
-  var _super = _createSuper(App);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _super.call(this); // Most of this moves to redux :/
-    // this.state = {
-    //     dice: [],
-    //     total: 0
-    // }
-    // this.get1to6 = this.get1to6.bind(this);
-    // this.rerollDie = this.rerollDie.bind(this);
-    // this.rerollAll = this.rerollAll.bind(this);
-  } // This is now in redux?
-  // componentDidMount() {
-  // let numberOfDice = this.get1to6();
-  // let dice = [];
-  // let total = 0;
-  // for (let i = 0; i < numberOfDice; i++) {
-  //     let value = this.get1to6();
-  //     dice.push({value: value});
-  //     total = total + value;
-  // }
-  // this.setState({
-  //     dice: dice,
-  //     total: total
-  // })
-  // }
-  // No longer to be used - Redux now?
-  // get1to6() {
-  //     return (Math.floor(Math.random() * Math.floor(6)))+1;
-  // }
-  // rerollDie(key) {
-  //     let dice = this.state.dice;
-  //     let original = dice[key].value;
-  //     let newVal = this.get1to6();
-  //     let difference = newVal - original;
-  //     dice[key] = {value: newVal};
-  //     let newTotal = this.state.total + difference;
-  //     this.setState({
-  //         dice:dice,
-  //         total: newTotal
-  //     });
-  // }
-  // Moving to redux?
-  // rerollAll()
-  // {
-  //     let dice = [];
-  //     this.state.dice.map((die, key) => {
-  //         dice[key] = {value: this.get1to6()}
-  //     });
-  //     this.setState({dice:dice});
-  // }
-
-
-  _createClass(App, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      var dice = this.props.dice.map(function (die, key) {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Die_Die__WEBPACK_IMPORTED_MODULE_2__["default"], {
-          key: key,
-          index: key,
-          value: die.value,
-          reroll: function reroll() {
-            return _this.props.reroll(key);
-          }
-        });
-      });
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        onClick: this.props.setupDice
-      }, "Setup Dice"), dice, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Total: ", this.props.total), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        onClick: this.props.rerollAll
-      }, "Reroll All"));
-    }
-  }]);
-
-  return App;
-}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+function App(props) {
+  var dice = props.dice.map(function (die, key) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Die_Die__WEBPACK_IMPORTED_MODULE_2__["default"], {
+      key: key,
+      index: key,
+      value: die.value,
+      reroll: function reroll() {
+        return props.reroll(key);
+      }
+    });
+  });
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    onClick: props.setupDice
+  }, "Setup Dice"), dice, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Total: ", props.total), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    onClick: props.rerollAll
+  }, "Reroll All"));
+}
 
 var mapStateToProps = function mapStateToProps(state) {
   return {
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,83 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Die from './Die/Die';
 import { connect } from 'react-redux';
 
-class App extends Component {
+function App(props) {
 
-    constructor () {
-        super();
-        // Most of this moves to redux :/
-        // this.state = {
-        //     dice: [],
-        //     total: 0
-        // }
-        // this.get1to6 = this.get1to6.bind(this);
-        // this.rerollDie = this.rerollDie.bind(this);
-        // this.rerollAll = this.rerollAll.bind(this);
-    }
-
-    // This is now in redux?
-    // componentDidMount() {
-        // let numberOfDice = this.get1to6();
-        // let dice = [];
-        // let total = 0;
-        // for (let i = 0; i < numberOfDice; i++) {
-        //     let value = this.get1to6();
-        //     dice.push({value: value});
-        //     total = total + value;
-        // }
-        // this.setState({
-        //     dice: dice,
-        //     total: total
-        // })
-    // }
-
-    // No longer to be used - Redux now?
-    // get1to6() {
-    //     return (Math.floor(Math.random() * Math.floor(6)))+1;
-    // }
-
-    // rerollDie(key) {
-    //     let dice = this.state.dice;
-
-    //     let original = dice[key].value;
-    //     let newVal = this.get1to6();
-    //     let difference = newVal - original;
-
-    //     dice[key] = {value: newVal};
-    //     let newTotal = this.state.total + difference;
-
-    //     this.setState({
-    //         dice:dice,
-    //         total: newTotal
-    //     });
-    // }
-
-    // Moving to redux?
-    // rerollAll()
-    // {
-    //     let dice = [];
-    //     this.state.dice.map((die, key) => {
-    //         dice[key] = {value: this.get1to6()}
-    //     });
-    //     this.setState({dice:dice});
-    // }
-
-    render() {
-        let dice = this.props.dice.map((die, key) =>
-            <Die key={key} index={key} value={die.value} reroll={() => this.props.reroll(key)}></Die>
-        );
-        return (
-            <div>
-                <button onClick={this.props.setupDice}>Setup Dice</button>
-                {dice}
-                <p>Total: {this.props.total}</p>
-                <button onClick={this.props.rerollAll}>Reroll All</button>
-            </div>
-        );
-
-    }
+    let dice = props.dice.map((die, key) =>
+        <Die key={key} index={key} value={die.value} reroll={() => props.reroll(key)}></Die>
+    );
+    return (
+        <div>
+            <button onClick={props.setupDice}>Setup Dice</button>
+            {dice}
+            <p>Total: {props.total}</p>
+            <button onClick={props.rerollAll}>Reroll All</button>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
